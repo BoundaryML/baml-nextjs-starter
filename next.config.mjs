ee75a36de381a3524b0bdb65116ceb72b1a6ddcf
Vaibhav Gupta <vbv@gloo.chat>
2024-03-05 15:48:21 -0800

    Adding project configured for nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+        config.module.rules.push({
+          test: /\.node$/,
+          use: [
+            {
+              loader: "nextjs-node-loader",
+              options: {
+                outputPath: config.output.path
+              }
+            },
+          ],
+        });
+        return config;
+      },
+};
 
 export default nextConfig;
