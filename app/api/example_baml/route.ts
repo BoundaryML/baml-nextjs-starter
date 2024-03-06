1ab6b94aa82e678edfab1a5fe7c9320c74fa8844
Aaron Villalpando <aaron@boundaryml.com>
2024-03-06 11:15:33 -0800

    Test with napi-rs

--- app/api/example_baml/route.ts
+++ app/api/example_baml/route.ts
@@ -1,14 +1,16 @@
 export const dynamic = 'force-dynamic'
 
-import b from "@/baml_client"
+import { plus100 } from "@boundaryml/baml-core-napi"
 
 export async function POST(request: Request) {
-    const result = await b.ClassifyMessage({
-        message: "Hello from BAML!",
-        message_date: "2021-01-01T00:00:00Z",
-    });
+    // const result = await b.ClassifyMessage({
+    //     message: "Hello from BAML!",
+    //     message_date: "2021-01-01T00:00:00Z",
+    // });
 
-    return Response.json(result);
+    return Response.json({
+        result: plus100(100)
+    });
 }
 
 // export async function POST(request: Request) {
