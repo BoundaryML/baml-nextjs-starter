6f08983c57c8efa5a5b1db9fcc664401e1d78d0c
Vaibhav Gupta <vbv@gloo.chat>
2024-03-07 22:44:51 -0800

    Adding remainder of next-js example

--- app/api/example_baml/route.ts
+++ app/api/example_baml/route.ts
@@ -1,19 +1,12 @@
 export const dynamic = 'force-dynamic'
 
-import { plus100 } from "@boundaryml/baml-core-napi"
+import b from '@/baml_client'
 
 export async function POST(request: Request) {
-    // const result = await b.ClassifyMessage({
-    //     message: "Hello from BAML!",
-    //     message_date: "2021-01-01T00:00:00Z",
-    // });
-
-    return Response.json({
-        result: plus100(100)
+    const result = await b.ClassifyMessage({
+        message: "Hello from BAML!",
+        message_date: "2021-01-01T00:00:00Z",
     });
-}
 
-// export async function POST(request: Request) {
-//     const res = await request.json()
-//     return Response.json({ res })
-// }
+    return Response.json(result);
+}
